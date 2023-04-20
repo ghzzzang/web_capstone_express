131cb149af6dd26924a9ebf6ca2be660e99aa6be
Jihoon Kim <ghzzzang@naver.com>
2023-04-20 21:57:02 +0900

    feat: modify

--- main.js
+++ main.js
@@ -4,7 +4,9 @@
 //  변수 설정(포트,패키지)
 
 const port = 3000,
+    // 라우트에 해당하는 함수가 있는 모듈
     homeController = require("./controller/homeController"),
+    // 에러 발생시 호출되는 함수가 있는 모듈
     errorController = require("./controller/errorController"),
     express = require("express"),
     app = express(),
@@ -12,12 +14,12 @@ const port = 3000,
 
 
 // 정적 파일 설정 
-// 정적 파일이 있는 폴더를 설정해줌으로써 ejs 파일이 이를 확인 할 수 있다.
+// 정적 파일이 있는 폴더를 설정해줌으로써 ejs 파일이 이를 확인해 적용할 수 있다. 
 app.use(express.static('public'));
 
 
 // 포트 설정 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || port);
 
 // body-parser 추가 
 app.use(
@@ -29,10 +31,11 @@ app.use(express.json());
 
 
 
-
 // *********** ejs와 layouts를 사용하기 위한 세팅 ******* 
 
+// 뷰 엔진을 ejs로 설정 
 app.set("view engine", "ejs");
+// layout.ejs 내용이 기본 템플릿이 된다.(웹사이트의 기본 형태)
 app.use(layouts);
 
 
@@ -43,7 +46,7 @@ app.get("/courses", homeController.showCourses);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedContactForm);
 
-// 에러처리 라우트  생성(기존 라우트 아래에 생성)
+// 에러처리 라우트  생성 (기존 라우트 아래에 생성)
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
